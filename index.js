64369579bddf8eb9f46aacd7e4fa1830247175cf
Doan Anh Quan <doananhquan.bdg@gmail.com>
2024-01-26 20:06:24 +0700

    update

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-const imgs = ["bob.png", "among.png", "duolingo.jpg", "rob.png"];
-function changImgSource() {
-  var random = Math.floor(Math.random() * imgs.length);
-  var randomimgsrc = imgs[random];
+// const imgs = ["bob.png", "among.png", "duolingo.jpg", "rob.png"];
+// function changImgSource() {
+//   var random = Math.floor(Math.random() * imgs.length);
+//   var randomimgsrc = imgs[random];
 
-  var image = document.getElementById("img");
+//   var image = document.getElementById("img");
 
-  image.src = randomimgsrc;
-  window.location.href = `https://doananhquan.github.io/randomimg/imgs/${randomimgsrc}`;
-}
-window.onload = changImgSource;
+//   image.src = randomimgsrc;
+//   window.location.href = `https://doananhquan.github.io/randomimg/imgs/${randomimgsrc}`;
+// }
+// window.onload = changImgSource;
+document.addEventListener("DOMContentLoaded", function () {
+  const imgs = [
+    "./imgs/bob.png",
+    "./imgs/among.png",
+    "./imgs/duolingo.jpg",
+    "./imgs/rob.png",
+  ];
+  function getRandomIndex(array) {
+    return Math.floor(Math.random() * array.length);
+  }
+
+  function setRandomImage() {
+    const randomIndex = getRandomIndex(imgs);
+    const randomImageURL = imgs[randomIndex];
+    const randomImageElement = document.getElementById("img");
+    randomImageElement.src = randomImageURL;
+
+    // const newURL = window.location.origin + "/" + randomImageURL;
+    const newURL = `https://doananhquan.github.io/randomimg/${randomImageURL}`;
+    window.location.href = newURL;
+  }
+
+  setRandomImage();
+
+  document.getElementById("img").addEventListener("click", setRandomImage);
+});
